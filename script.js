8549497984de22dd10524e34e44901f0a645c07c
ryuun1corn <yuda.arif.prasojo@gmail.com>
2023-09-07 20:56:10 +0700

    projects card only works on desktop

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const navBtns = document.getElementsByClassName("scroll-link");
 const scrollElements = document.getElementsByClassName("scroll-element");
+const projectsLinks = document.getElementsByClassName("projects-card");
 
 for (let i=0; i<navBtns.length; i++) {
     navBtns[i].addEventListener('click', function (e) {
@@ -13,4 +14,14 @@ function scrollToElement(instance) {
         scrollElements[instance].scrollIntoView({ behavior: "smooth"});
     }
     console.log(scrollElements[instance]);
+}
+
+function isMobileDevice() {
+    return /Mobi|Android/i.test(navigator.userAgent);
+}
+
+if (isMobileDevice()) {
+    for (let i=0; i<projectsLinks.length; i++) {
+        projectsLinks[i].href = "";
+    }
 }
